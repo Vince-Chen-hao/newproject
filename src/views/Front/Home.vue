29a227d2d6f6f6058c9d953b0715c2db1ebedc55
Leah Lin <leahlin912@gmail.com>
2024-12-11 20:42:11 +0800

    提交說明

--- src/views/Front/Home.vue
+++ src/views/Front/Home.vue
@@ -69,23 +69,23 @@
       <!-- SHOWCASE SLIDER -->
       <section id="showcase" style="margin-top: 80px">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-          <!--  <ol class="carousel-indicators">
+           <ol class="carousel-indicators">
            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          </ol>-->
+          </ol>
           <div class="carousel-inner">
-           <!--  <div class="carousel-item active w-100">
-              <a href="https://lin.ee/v0Ta2kq" target="_blank">
-              <img src="../../assets/images/home_line_ad.png" class="d-block w-100" alt="..." />
+            <div class="carousel-item active w-100">
+              <a href="https://lin.ee/dhcKcoE" target="_blank">
+              <img src="../../assets/images/home1211.png" class="d-block w-100" alt="..." />
               </a>
-            </div> -->
+            </div>
 
             <div class="carousel-item active w-100">
               <img src="../../assets/images/home13.png" class="d-block w-100" alt="..." />
             </div>
           </div>
 
-          <!--
+
           <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
@@ -93,7 +93,7 @@
           <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
-          </a>-->
+          </a>
         </div>
       </section>
 
