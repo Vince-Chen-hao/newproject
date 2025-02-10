457b26cbd7a62ff2ed9ec76e317c86a3ff7c398f
Leah Lin <leahlin912@gmail.com>
2025-02-10 21:32:56 +0800

    提交信息

--- src/views/Front/Home.vue
+++ src/views/Front/Home.vue
@@ -69,10 +69,10 @@
       <!-- SHOWCASE SLIDER -->
       <section id="showcase" style="margin-top: 80px">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-           <ol class="carousel-indicators">
+           <!-- <ol class="carousel-indicators">
            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-             <!--  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>-->
-          </ol>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          </ol> -->
           <div class="carousel-inner">
             <!-- <div class="carousel-item active w-100">
               <a href="https://lin.ee/dhcKcoE" target="_blank">
