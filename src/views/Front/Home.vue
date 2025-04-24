97035520c1066ba037363635d79ea9811aae3e08
CHANCHAO\vince <vince@chanchao.com.tw>
2025-04-24 15:38:28 +0800

    新增手機版輪播圖片

--- src/views/Front/Home.vue
+++ src/views/Front/Home.vue
@@ -68,6 +68,42 @@
 
       <!-- SHOWCASE SLIDER -->
       <section id="showcase" style="margin-top: 80px">
+
+        <div class="MbDevice">
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+           <ol class="carousel-indicators">
+           <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active w-100">
+              <a href="https://lin.ee/dhcKcoE" target="_blank">
+              <img src="../../assets/images/MotherDay_mb.jpg" class="d-block w-100" alt="..." />
+              </a>
+            </div>
+
+            <div class="carousel-item   w-100">
+              <a href="https://lin.ee/dhcKcoE" target="_blank">
+              <img src="../../assets/images/home13.png" class="d-block w-100" alt="..." />
+              </a>
+
+            </div>
+          </div>
+
+ 
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+
+        </div>
+
+        <div class="PcDevice">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
            <ol class="carousel-indicators">
            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -98,6 +134,8 @@
             <span class="sr-only">Next</span>
           </a>
         </div>
+        </div>
+
       </section>
 
       <section id="CategoryList">
