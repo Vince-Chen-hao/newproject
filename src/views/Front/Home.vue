39621c244ac3c33cce2760b186a62e75cc01f94d
CHANCHAO\vince <vince@chanchao.com.tw>
2025-04-24 15:55:52 +0800

    更新行動版的輪播尺寸

--- src/views/Front/Home.vue
+++ src/views/Front/Home.vue
@@ -71,10 +71,7 @@
 
         <div class="MbDevice">
           <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-           <ol class="carousel-indicators">
-           <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          </ol>
+           
           <div class="carousel-inner">
             <div class="carousel-item active w-100">
               <a href="https://lin.ee/dhcKcoE" target="_blank">
@@ -84,7 +81,7 @@
 
             <div class="carousel-item   w-100">
               <a href="https://lin.ee/dhcKcoE" target="_blank">
-              <img src="../../assets/images/home13.png" class="d-block w-100" alt="..." />
+              <img src="../../assets/images/home13_mb.png" class="d-block w-100" alt="..." />
               </a>
 
             </div>
